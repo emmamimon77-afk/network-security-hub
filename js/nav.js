88511dbe4db8b31c10c90807d2c0cadc2ea5ac59
Your Name <your-email@gmail.com>
2026-04-26 20:40:26 +0200

    🗺️ Add complete Static Routing lesson - syntax, AD, floating routes, default routes, 3-router lab

--- js/nav.js
+++ js/nav.js
@@ -19,7 +19,7 @@ function loadNavigation() {
             <a href="javascript:void(0)">🔀 Routing & Switching ▾</a>
             <div class="nav-dropdown-content">
                 <a href="../routing-switching/vlan-config.html">🏷️ VLAN Configuration</a>
-                <a href="#">🗺️ Static Routing (Coming Soon)</a>
+                <a href="../routing-switching/static-routing.html">🗺️ Static Routing</a>
                 <a href="#">🔄 OSPF (Coming Soon)</a>
                 <a href="#">⚡ EIGRP (Coming Soon)</a>
             </div>
